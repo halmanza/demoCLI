748c05f743fb67dec4d7b8ce61c1b9522de44e09
Anthony Almanza <anthonya771@gmail.com>
2022-08-05 00:37:24 -0700

    added eslint and rules to project

--- src/commandLineOptions.ts
+++ src/commandLineOptions.ts
@@ -7,40 +7,37 @@ type cliOptions =
     "createEncodedLink": boolean
 }
 
-const commandLineOptions = (userInput:string):cliOptions=>
-{
-    let options:cliOptions=
+const commandLineOptions = (userInput:string):cliOptions => {
+  const options:cliOptions =
     {
-        "convertToBinary": false,
-        "addDelimiter" : false,
-        "convertToBase64String": false,
-        "convertToStream": false,
-        "createEncodedLink": false
+      convertToBinary: false,
+      addDelimiter: false,
+      convertToBase64String: false,
+      convertToStream: false,
+      createEncodedLink: false
     }
-    switch(userInput)
-    {
-        case "--binary":
-            options.convertToBinary = true;
-            break;
-        case "--binary -d":
-            options.addDelimiter = true;
-            options.convertToBinary = true;
-            break;
-        case "--base64":
-            options.convertToBase64String = true;
-            break;
-        case "--stream":
-            options.convertToStream = true;
-            break;
-        case "--encoded":
-            options.createEncodedLink = true;
-            break;
-        default:
-            throw new Error("Improper options parameter.");
-    }
+  switch (userInput) {
+    case "--binary":
+      options.convertToBinary = true
+      break
+    case "--binary -d":
+      options.addDelimiter = true
+      options.convertToBinary = true
+      break
+    case "--base64":
+      options.convertToBase64String = true
+      break
+    case "--stream":
+      options.convertToStream = true
+      break
+    case "--encoded":
+      options.createEncodedLink = true
+      break
+    default:
+      throw new Error("Improper options parameter.")
+  }
 
-    return options;
+  return options
 }
 
-
-export {commandLineOptions, cliOptions};
+export { commandLineOptions, cliOptions }
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,34 @@
 #!/usr/bin/env node
-import fs from 'fs/promises';
-import {commandLineOptions, cliOptions} from "./commandLineOptions.js";
-import CentralCommand from './CentralCommand.js';
+import fs from "fs/promises";
+import { commandLineOptions, cliOptions } from "./commandLineOptions.js";
+import CentralCommand from "./CentralCommand.js";
 
-const fileName:string = process.argv[2];
-const optionsArgument:string = process.argv[3];
-const optionsAddedAttribute:string = process.argv[4];
+const fileName: string = process.argv[2];
+const optionsArgument: string = process.argv[3];
+const optionsAddedAttribute: string = process.argv[4];
 
-try
-{
-        let file:Promise<string> = fs.readFile(fileName, {encoding: "utf-8"});
-        let stringFromFile = await file;
-        let formattedSecondArgument:string = optionsAddedAttribute ? 
-            `${optionsArgument.toLowerCase().trim()} ${optionsAddedAttribute.toLowerCase().trim()}`: optionsArgument.toLowerCase();
-            
-        const options:cliOptions = commandLineOptions(formattedSecondArgument);
-        const commandCenter = CentralCommand.GetInstance(options);
-        
-        const userRequestOutput = commandCenter.ProcessRequest(stringFromFile);
-        console.log(userRequestOutput);
+try {
+  const file: Promise<string> = fs.readFile(fileName, { encoding: "utf-8" });
+  const stringFromFile = await file;
+  const formattedSecondArgument: string = optionsAddedAttribute
+    ? `${optionsArgument.toLowerCase().trim()} ${optionsAddedAttribute
+        .toLowerCase()
+        .trim()}`
+    : optionsArgument.toLowerCase();
+
+  const options: cliOptions = commandLineOptions(formattedSecondArgument);
+  const commandCenter = CentralCommand.GetInstance(options);
+
+  const userRequestOutput = commandCenter.ProcessRequest(stringFromFile);
+  console.log(userRequestOutput);
+} catch (error: unknown) {
+  let message: string = "";
+
+  if (error instanceof Error) message = error.message;
+
+  if (message.includes("properties") && message.includes("undefined")) {
+    console.error("improper property flag.");
+  } else {
+    console.error(message);
+  }
 }
-catch(error:unknown)
-{
-    let message:string = "";
-
-    if(error instanceof Error) message = error.message;
-    
-    if(message.includes('properties') && message.includes('undefined'))
-    {
-        console.error("improper property flag.")
-    }
-    else
-    {
-        console.error(message);
-    }
-}
-
-
